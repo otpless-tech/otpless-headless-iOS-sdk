f8bf8a6b3c7be52b49120a1c5dd3b8fa5e723139
dev-otpless-tech <121038664+dev-otpless-tech@users.noreply.github.com>
2025-05-07 01:16:59 +0530

    doc changes (#17)

--- Sources/OtplessBM/utils/DeviceInfoUtils.swift
+++ Sources/OtplessBM/utils/DeviceInfoUtils.swift
@@ -118,7 +118,7 @@ class DeviceInfoUtils : @unchecked Sendable {
             params["tsid"] = tsid
         }
         
-        params["sdkVersion"] = "1.1.3"
+        params["sdkVersion"] = "1.1.4"
         
         params["osVersion"] = os.majorVersion.description + "." + os.minorVersion.description
         params["hasWhatsapp"] = hasWhatsApp.description

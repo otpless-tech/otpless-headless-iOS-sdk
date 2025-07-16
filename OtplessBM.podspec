5a70c15ee4b281ed6f721c15693a2ec661f1f962
dev-otpless-tech <121038664+dev-otpless-tech@users.noreply.github.com>
2025-07-16 16:17:11 +0530

    version bump 117

--- OtplessBM.podspec
+++ OtplessBM.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'OtplessBM'
-  s.version          = '1.1.6'
+  s.version          = '1.1.7'
   s.summary          = 'Standalone SDK for Otpless Headless functionality.'
 
   s.description      = <<-DESC
--- Sources/OtplessBM/utils/EventHelper.swift
+++ Sources/OtplessBM/utils/EventHelper.swift
@@ -13,7 +13,7 @@ func sendEvent(event: EventConstants, extras: [String: String] = [:], musId: Str
         var params = [String: String]()
         params["event_name"] = event.rawValue
         params["platform"] = "iOS-headless"
-        params["sdk_version"] = "1.1.6"
+        params["sdk_version"] = "1.1.7"
         params["mid"] = Otpless.shared.merchantAppId
         params["event_timestamp"] = Utils.formatCurrentTimeToDateString()
         

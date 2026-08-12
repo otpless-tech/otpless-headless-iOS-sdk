382b7d0ffac21c795db88b62b94779c6ae46ae27
Sachin Chandil <sachin.chandil@meesho.com>
2026-08-12 17:30:43 +0530

    fix: declare Facebook SwiftPM dependencies

--- Package.swift
+++ Package.swift
@@ -14,12 +14,15 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "17.0.0"),
         .package(url: "https://github.com/otpless-tech/otpless-event-io-ios.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "OtplessBM",
             dependencies: [
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
                 .product(name: "OtplessEventIO", package: "otpless-event-io-ios")
             ]
         ),
